f2269cf79f0579bc84b919947b98a263eab704fd
eithri <guillermo.paez@uptc.edu.co>
2021-10-15 03:27:00 -0500

    added about page

---
+++ Web/mevn/client/src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card
+    class="mx-auto my-12"
+    max-width="350"
+  >
+    <v-img
+      :src="urlimg"
+      height="200px"
+    />
+
+    <v-card-title>
+      {{username}}
+    </v-card-title>
+
+    <v-card-subtitle>
+      {{description}}
+    </v-card-subtitle>
+  </v-card>
+</template>
+
+<script>
+export default {
+ props: ['username', 'urlimg', 'description']
+}
+</script>
--- Web/mevn/client/src/views/About.vue
+++ Web/mevn/client/src/views/About.vue
@@ -1,3 +1,20 @@
 <template>
-<h1>About Page</h1>
+<v-container class="d-flex flex-row flex-wrap m-6">
+  
+  <Card username="Guillermo Paez Guerra" urlimg="../assets/GLL.jpg" description="Front-End developer"/>
+  <Card username="Wilson Andres Carvajal Barreto" urlimg="../assaets/AC.jpeg" description="Front-End developer"/>
+  <Card username="Duvan Rodelo" urlimg="../assets/DUVAN.jpeg" description="Front-End developer"/>
+  <Card username="Diego Rojas Avellaneda" urlimg="../assets/DG.jpg" description="Front-End developer"/>
+  <Card username="Manuel Alejandro Rativa Rodriguez" urlimg="@/assets/Manuel.jpeg" description="Front-End developer"/>
+</v-container>
 </template>
+
+<script>
+import Card from "../components/Card.vue";
+ 
+export default {
+ components: {
+   Card
+ }
+}
+</script>
