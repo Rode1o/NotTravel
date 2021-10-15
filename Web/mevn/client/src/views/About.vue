44fa5563a1e701ee68ad3f4ec4c933ac5e39dc0d
Guillermo Paez Guerra <guillermo.paez@uptc.edu.co>
2021-10-15 08:30:32 -0500

    Updated images AboutUs.vue

--- Web/mevn/client/src/views/About.vue
+++ Web/mevn/client/src/views/About.vue
@@ -1,11 +1,10 @@
 <template>
 <v-container class="d-flex flex-row flex-wrap m-6">
-  
-  <Card username="Guillermo Paez Guerra" urlimg="../assets/GLL.jpg" description="Front-End developer"/>
-  <Card username="Wilson Andres Carvajal Barreto" urlimg="../assaets/AC.jpeg" description="Front-End developer"/>
-  <Card username="Duvan Rodelo" urlimg="../assets/DUVAN.jpeg" description="Front-End developer"/>
-  <Card username="Diego Rojas Avellaneda" urlimg="../assets/DG.jpg" description="Front-End developer"/>
-  <Card username="Manuel Alejandro Rativa Rodriguez" urlimg="@/assets/Manuel.jpeg" description="Front-End developer"/>
+  <Card username="Guillermo Paez Guerra" urlimg="https://i.ibb.co/KLfK37B/GLL.jpg" description="Front-End developer"/>
+  <Card username="Wilson Andres Carvajal Barreto" urlimg="https://i.ibb.co/jb8F3xP/AC.jpg" description="Front-End developer"/>
+  <Card username="Duvan Rodelo" urlimg="https://i.ibb.co/PrNWrFK/DUVAN.jpg" description="Front-End developer"/>
+  <Card username="Diego Rojas Avellaneda" urlimg="https://i.ibb.co/FqLtLJv/DG.jpg" description="Front-End developer"/>
+  <Card username="Manuel Alejandro Rativa Rodriguez" urlimg="https://i.ibb.co/ZBWbmpF/Manuel.jpg" description="Front-End developer"/>
 </v-container>
 </template>
 
